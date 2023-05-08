c057d838cea3e3e8d108085ad742cba2d751cb43
EzeChiiii <ciwu@uncc.edu>
2023-05-08 12:37:50 -0400

    Create part1.js

---
+++ lab5-starter/js/part1.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Returns a string of the count and Bet or Hold
+ * @param {array} cards - an array of cards
+ * @returns {string} - message 
+ */
+function countCards(cards) {
+    let number = 0;
+    cards.forEach(function(card) {
+        if ([2, 3, 4, 5, 6].includes(card)) {
+            number++;
+        } else if ([10, 'J', 'Q', 'K', 'A'].includes(card)) {
+            number--;
+        }
+    });
+    return number > 0 ? number + ' Bet' : number + ' Hold';
+}
+
+
+
+
+// uncomment following test code after implementing the function
+console.log(countCards([2, 3, 7, 'K', 'A']));
+console.log(countCards([2, 3, 4, 5, 6]));
+console.log(countCards([7, 8, 9]));
+console.log(countCards([10, 'J', 'Q', 'K', 'A']));
+console.log(countCards([3, 7, 'Q', 8, 'A']));
+console.log(countCards([2, 2, 10]));
+console.log(countCards([2, 9, 'J', 2, 7]));
+console.log(countCards([3, 2, 'A', 10, 'K']));
+
